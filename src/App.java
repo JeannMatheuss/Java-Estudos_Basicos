a5da68bb3ced1b959f391fa9a8f79a2c92e8ef7d
Jean <jeanmatheus0612@gmail.com>
2024-10-13 14:49:03 -0300

    Criando novos tipos de variaveis

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: "+ anoDeLancamento);
+
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        System.out.println("Está incluido no plano? " + incluidoNoPlano);
+        System.out.println("Nota do filme: " + notaDoFilme);
     }
 }
